d1ac65c6bf3c346f0c00b57c22404db358555ee1
Mahak Gupta <mahakgupta10a24@gmail.com>
2025-06-15 23:23:05 +0530

    Added time concept

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,6 +8,16 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
-function calculateTime(n) {
-    return 0.01;
+function calculateTime (n) {
+  let sum = 0
+  let start = new Date()
+  for (let i = 0; i < n; i++) {
+    sum += i
+  }
+  let end = new Date()
+  let time = (end - start) / 1000
+  console.log(sum)
+  return time
 }
+
+console.log(calculateTime(100000))
